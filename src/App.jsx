4f627697283bfa819788160550a4510ba377ac17
Ansh Jaiswal <anshjaiswalm249@gmail.com>
2026-05-25 00:36:22 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -88,6 +88,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
 
